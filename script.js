9593e3772a757efd09588b3a9ee14ba61de7dae1
therealrajan <therealrajan@gmail.com>
2025-03-29 16:48:51 +0545

    Updated image sources 650+, streamlined card-rendering, load more feature

--- script.js
+++ script.js
@@ -1,130 +1,139 @@
-const pokeContainer = document.querySelector('.poke-container');
-const pokemonCount = 350
+const pokeContainer = document.querySelector(".poke-container")
+const loadMoreButton = document.querySelector(".load-more") // Select the button from the HTML
+
+const pokemonCount = 1025 // Total number of Pokémon available
+const batchSize = 100 // Number of Pokémon to load per batch
+let currentCount = 0 // Tracks how many Pokémon have been loaded
 
 const colors = {
-    fire:'orange',
-    grass:'lightgreen',
-    electric:'yellow',
-    water:'#70ffea',
-    ground:'darkgrey',
-    rock:'grey',
-    fairy:'pink',
-    poison:'greenyellow',
-    bug:'#94ecbe',
-    dragon:'orange',
-    psychic:'#7c7db6', 
-    flying:'#fcca46',
-    fighting:'darkgrey',
-    normal:'lightgrey',
-    ice:'#00f2f2',
-    dark: '#4f7ecf',
-    ghost: '#7685a7',
-    steel: 'steelblue',
-
+  fire: "orange",
+  grass: "lightgreen",
+  electric: "yellow",
+  water: "#70ffea",
+  ground: "darkgrey",
+  rock: "grey",
+  fairy: "pink",
+  poison: "greenyellow",
+  bug: "#94ecbe",
+  dragon: "orange",
+  psychic: "#7c7db6",
+  flying: "#fcca46",
+  fighting: "darkgrey",
+  normal: "lightgrey",
+  ice: "#00f2f2",
+  dark: "#4f7ecf",
+  ghost: "#7685a7",
+  steel: "steelblue",
 }
-//Getting the keys of the colors Object to access the color value later
+
 const mainTypes = Object.keys(colors)
-// console.log(mainTypes)
 
-//Sending API request for each Pokemon
-const fetchPokemon = async () => {
-    for (let i = 1; i<= pokemonCount; i++){
-       await getPokemon(i)
+// Fetch a batch of Pokémon data
+const fetchPokemonBatch = async () => {
+  // Disable the "Load More" button while loading
+  loadMoreButton.disabled = true
+  loadMoreButton.textContent = "Loading..."
+
+  const start = currentCount + 1
+  const end = Math.min(currentCount + batchSize, pokemonCount)
+
+  for (let i = start; i <= end; i++) {
+    try {
+      const pokemon = await getPokemon(i)
+      const pokemonEl = createPokemonCard(pokemon)
+      pokeContainer.appendChild(pokemonEl) // Append each card as it is created
+    } catch (err) {
+      console.error(`Failed to fetch Pokémon with ID ${i}:`, err)
     }
+  }
+
+  currentCount = end
+
+  // Re-enable the "Load More" button if there are more Pokémon to load
+  if (currentCount < pokemonCount) {
+    loadMoreButton.disabled = false
+    loadMoreButton.textContent = "Load More"
+  } else {
+    // Disable the button permanently if all Pokémon are loaded
+    loadMoreButton.disabled = true
+    loadMoreButton.textContent = "No More Pokémon"
+  }
 }
 
-// Send request to PokeApi and get Json data about each pokemon
-const getPokemon = async (id) =>{
-    try{
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-        const response = await axios.get(url)
-        const data = response.data
-        console.log(data)
-        // console.log(data)
-        createPokemonCard(data)
-    }
-    catch(err){
-        console.log(err)
-    }
+// Fetch a single Pokémon by ID
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+  const response = await axios.get(url)
+  return response.data
 }
 
-//Create pokemon info card in the DOM for each pokemon
-const createPokemonCard = (pokemon) =>{
-    const pokemonEl = document.createElement('div')
-    pokemonEl.classList.add('pokemon')
+// Create a Pokémon card
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div")
+  pokemonEl.classList.add("pokemon")
 
-    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-    const id = pokemon.id.toString().padStart(3, '0')
-    const pokeTypes = pokemon.types.map(typeKind =>typeKind.type.name)
-    const image= pokemon.sprites.other.dream_world.front_default
-    const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1)
-    const color = colors[type]
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+  const id = pokemon.id.toString().padStart(3, "0")
+  const pokeTypes = pokemon.types.map((typeKind) => typeKind.type.name)
 
+  // Use different URLs based on the Pokémon ID
+  const image =
+    pokemon.id >= 650
+      ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+      : pokemon.sprites.other.dream_world.front_default ||
+        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`
 
-    pokemonEl.innerHTML = 
-    `          
+  const fallbackImage =
+    "https://www.pngmart.com/files/22/Unown-Pokemon-PNG-Isolated-HD-Pictures.png"
+
+  const type = mainTypes.find((type) => pokeTypes.includes(type))
+  const color = colors[type]
+
+  pokemonEl.innerHTML = `
         <span class="number">#${id}</span>
         <span class="type-icon"></span>
         
-        <div class="img-container">
-            <img loading="lazy" src="${image}" alt="${name}">    
+        <div class="img-container">   
+            <img loading="lazy" src="${image}" alt="${name}" onerror="this.src='${fallbackImage}'">    
         </div>
         <div class="info">
-            
-            <h3 class="name">${(name)}</h3>
-            
+            <h3 class="name">${name}</h3>
             <div class="extra-info">
-                <div> <small> Weight</small> <h5 class="weight"> ${pokemon.weight/10} kg </h5>
-                </div>
-                <div> <small> Height</small> <h5 class="height"> ${pokemon.height/10} m</h5></div>      
+                <div><small>Weight</small><h5 class="weight">${
+                  pokemon.weight / 10
+                } kg</h5></div>
+                <div><small>Height</small><h5 class="height">${
+                  pokemon.height / 10
+                } m</h5></div>
             </div>
-            
-            <div class="type-data"> <small> Type:</small> <h5 class="type">${getPokemonType(pokeTypes)}</h5></div>
-            
+            <div class="type-data"><small>Type:</small><h5 class="type">${getPokemonType(
+              pokeTypes
+            )}</h5></div>
         </div>
-
     `
 
-     const typeColor = pokemonEl.querySelector('.type-icon')
-     const imageBg = pokemonEl.querySelector('.img-container')
-     pokemonEl.style.border = `2px solid ${color}`
-     typeColor.style.backgroundColor = color
-     typeColor.setAttribute('title', type)
-     typeColor.style.boxShadow = `0 0 6px ${color}`
-     pokeContainer.appendChild(pokemonEl)
-         
+  const typeColor = pokemonEl.querySelector(".type-icon")
+  pokemonEl.style.border = `2px solid ${color}`
+  typeColor.style.backgroundColor = color
+  typeColor.setAttribute("title", type)
+  typeColor.style.boxShadow = `0 0 6px ${color}`
+
+  return pokemonEl
 }
 
-// Getting the type(s) for each pokemon
-function getPokemonType(pokeTypes){
-   if(pokeTypes.length == 1){
-      const pokeType = (pokeTypes[0])[0].toUpperCase() + pokeTypes[0].slice(1)
-        return pokeType
-   } 
-    else{ 
-        pokeType1 = (pokeTypes[0])[0].toUpperCase() + pokeTypes[0].slice(1)
-        pokeType2 = (pokeTypes[1])[0].toUpperCase() + pokeTypes[1].slice(1)
-        return pokeType1 + ' / ' + pokeType2
-    }
-
+// Get Pokémon type(s)
+function getPokemonType(pokeTypes) {
+  return pokeTypes
+    .map((type) => type[0].toUpperCase() + type.slice(1))
+    .join(" / ")
 }
 
+// Initial load of Pokémon
+fetchPokemonBatch()
 
-fetchPokemon()
+// Add event listener to the "Load More" button
+loadMoreButton.addEventListener("click", fetchPokemonBatch)
 
-const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-
-lazyImages.forEach(image => {
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        image.src = image.dataset.src;
-        observer.unobserve(image);
-      }
-    });
-  });
-
-  observer.observe(image);
-});
-
-console.log("All the cards were generated dynamically with the data from pokeapi.com. !")
+console.log(
+  "All the cards were generated dynamically with the data from pokeapi.com!"
+)
